8e8f9f01195fafb83fe2642ea6cbdefaa70b5680
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-03-05 21:05:18 +0200

    Add new method of finding a movie by index

--- src/main/java/dev/dyatlyukk/mymovies/repository/MovieRepository.java
+++ src/main/java/dev/dyatlyukk/mymovies/repository/MovieRepository.java
@@ -5,6 +5,11 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
+@Repository //data access layer
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
 }
